f9c1ae1b076fda8f8b3be376fde8a1b675aaa5b6
Anthony Cieri <penguinmillion@gmail.com>
2024-07-26 15:34:04 -0700

    feat: add inlet parameters

---
+++ Inlet.py
@@ -0,0 +1,16 @@
+
+import SimMath
+
+from SimMath import Vector
+
+"""
+The average mass of seawater (kg / m^3)
+"""
+
+density: float = 1024
+
+
+# TODO
+def get_density(point: Vector) -> float:
+
+    return density
